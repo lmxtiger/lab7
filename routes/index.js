1dc1dc41529be35224ba78fdfde5c2b83792e0f7
Minxuan Liu <realnimiliu@gmail.com>
2021-02-18 11:05:56 -0800

    google analytics enabled

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 var projects = require('../projects.json');
 
 /*
@@ -5,5 +6,11 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects["viewAlt"] = false;
   	response.render('index', projects);
 };
+
+exports.viewAlt = function(request, response){
+	projects["viewAlt"] = true;
+	response.render('index', projects);
+}
